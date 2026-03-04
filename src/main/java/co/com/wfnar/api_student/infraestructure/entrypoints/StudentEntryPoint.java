47059bf351dc0b2641ddc7ab8d1b1cb03fbe347f
William Felipe Naranjo <felipe.naranjo@ias.com.co>
2026-03-04 15:14:34 -0500

    se adiciona metodo by id

--- src/main/java/co/com/wfnar/api_student/infraestructure/entrypoints/StudentEntryPoint.java
+++ src/main/java/co/com/wfnar/api_student/infraestructure/entrypoints/StudentEntryPoint.java
@@ -4,6 +4,7 @@ import co.com.wfnar.api_student.domain.model.Student;
 import co.com.wfnar.api_student.domain.usecase.StudentUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +17,18 @@ public class StudentEntryPoint {
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        return studentUseCase.getAllStudents();
+        List<Student> students = studentUseCase.getAllStudents();
+        return students;
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudent(@PathVariable Integer id) {
+
+        List<Student> students = studentUseCase.getAllStudents();
+
+        return students.stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
