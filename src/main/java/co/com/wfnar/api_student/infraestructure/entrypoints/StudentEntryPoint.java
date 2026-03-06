d3d8eaad84f86c429188dc87c54b2621f11cfde2
William Felipe Naranjo <felipe.naranjo@ias.com.co>
2026-03-06 14:18:27 -0500

    Fin ejercicio 8

--- src/main/java/co/com/wfnar/api_student/infraestructure/entrypoints/StudentEntryPoint.java
+++ src/main/java/co/com/wfnar/api_student/infraestructure/entrypoints/StudentEntryPoint.java
@@ -3,7 +3,6 @@ package co.com.wfnar.api_student.infraestructure.entrypoints;
 import co.com.wfnar.api_student.domain.model.Student;
 import co.com.wfnar.api_student.domain.usecase.StudentUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +23,7 @@ public class StudentEntryPoint {
 
     @GetMapping
     public List<Student> getStudents() {
-        List<Student> students = studentUseCase.getAllStudents();
-        return students;
+        return studentUseCase.getAllStudents();
     }
 
     @GetMapping("/student/{id}")
@@ -43,110 +41,171 @@ public class StudentEntryPoint {
 
     @GetMapping("/basicas/grado11")
     public List<Student> getStudentsGrado11() {
-        List<Student> students = studentUseCase.getAllStudents().stream()
+        return studentUseCase.getAllStudents().stream()
                 .filter( student -> student.getGrade() == 11)
                 .toList();
-        return students;
     }
 
     @GetMapping("/basicas/nombres")
     public List<String> getNameStudents() {
-        List<String> students = studentUseCase.getAllStudents().stream()
-                .map( student -> student.getFirstName())
+        return studentUseCase.getAllStudents().stream()
+                .map(Student::getFirstName)
                 .toList();
-        return students;
     }
 
     @GetMapping("/basicas/mayora15")
     public List<Student> getStudentsMayorThat15() {
-        List<Student> students = studentUseCase.getAllStudents().stream()
+        return studentUseCase.getAllStudents().stream()
                 .filter( student -> student.getAge() > 15)
                 .toList();
-        return students;
     }
 
 
     // Transformación y ordenamiento
     @GetMapping("/transformacion/menoramayor")
     public List<Student> getStudentsOrderByAge() {
-        List<Student> students = studentUseCase.getAllStudents().stream()
+        return studentUseCase.getAllStudents().stream()
                 .sorted( Comparator.comparing(Student::getAge))
                 .toList();
-        return students;
     }
 
     @GetMapping("/transformacion/grupos")
     public List<String> getGroups() {
-        List<String> groups = studentUseCase.getAllStudents().stream()
-                .map( student -> student.getGroup())
+        return studentUseCase.getAllStudents().stream()
+                .map(Student::getGroup)
                 .distinct()
                 .toList();
-        return groups;
     }
 
     // Operaciones de agregación
     @GetMapping("/agregacion/ageaverage")
     public Double getAgeAverage() {
-        Double AgeAverage = studentUseCase.getAllStudents().stream()
+        return studentUseCase.getAllStudents().stream()
                 .mapToInt(Student::getAge)
                 .average()
                 .orElse(0);
-        return AgeAverage;
     }
 
     @GetMapping("/agregacion/studentsbygrade")
     public Map<Integer, Long> getTotalByGrade() {
-        Map<Integer, Long> totalByGroup = studentUseCase.getAllStudents().stream()
+
+        return studentUseCase.getAllStudents().stream()
                 .collect(Collectors.groupingBy(Student::getGrade , Collectors.counting()));
-
-
-        return totalByGroup;
     }
 
     @GetMapping("/agregacion/studentsbyethnic")
     public Map<String, Long> getTotalEthnicGroup() {
-        Map<String, Long> totalByEthnicGroup = studentUseCase.getAllStudents().stream()
+
+        return studentUseCase.getAllStudents().stream()
                 .collect(Collectors.groupingBy(Student::getEthnicGroup , Collectors.counting()));
-
-
-        return totalByEthnicGroup;
     }
 
     //Agrupaciones
     @GetMapping("/agrupacion/studentsbygrade")
     public Map<Integer, List<Student>> getStudentsByGrade() {
-        Map<Integer, List<Student>> studentsByGroup = studentUseCase.getAllStudents().stream()
+
+        return studentUseCase.getAllStudents().stream()
                 .collect(Collectors.groupingBy(Student::getGrade , Collectors.toList()));
-
-
-        return studentsByGroup;
     }
 
     @GetMapping("/agrupacion/studentsbygradeandgender")
     public Map<Integer, Map<String,List<Student>>> getStudentsByGradeAndGender() {
-        Map<Integer, Map<String,List<Student>>> studentsByGroupAndGender = studentUseCase.getAllStudents().stream()
+
+        return studentUseCase.getAllStudents().stream()
                 .collect(Collectors.groupingBy(Student::getGrade , (Collectors.groupingBy(Student::getGender, Collectors.toList()))));
-
-
-        return studentsByGroupAndGender;
     }
 
     //Transformaciones
     @GetMapping("/transformavanzadas/namebygrade")
     public Map<Integer, List<String>> getNameByGrade() {
-        Map<Integer, List<String>> namesByGroup = studentUseCase.getAllStudents().stream()
+
+
+        return studentUseCase.getAllStudents().stream()
                 .collect(Collectors.groupingBy(Student::getGrade , (Collectors.mapping(Student::getFirstName, Collectors.toList()))));
-
-
-        return namesByGroup;
     }
 
     @GetMapping("/transformavanzadas/mayorbygrade")
     public Map<Integer, Optional<Student>> getMayorAgeByGrade() {
-        Map<Integer, Optional<Student>> mayorAgeByGroup = studentUseCase.getAllStudents().stream()
+
+        return studentUseCase.getAllStudents().stream()
                 .collect(Collectors.groupingBy(Student::getGrade , (Collectors.maxBy(Comparator.comparing(Student::getAge)))));
-
-
-        return mayorAgeByGroup;
     }
+
+    //Particiones
+    @GetMapping("/partitions")
+    public Map<Boolean, List<Student>> getStudentsByAge() {
+
+        return studentUseCase.getAllStudents().stream()
+                .collect(Collectors.partitioningBy(student -> student.getAge() > 15));
+    }
+
+    //busquedas
+    @GetMapping("/busquedas/findfirst")
+    public Student getfirstStudent() {
+
+        return studentUseCase.getAllStudents().stream()
+                .filter(student -> student.getGrade() == 1)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @GetMapping("/busquedas/findanyindigenous")
+    public Boolean isAnyIndigenous() {
+
+        return studentUseCase.getAllStudents().stream()
+                .anyMatch(student -> student.getEthnicGroup().equals("Indigenous"));
+    }
+
+    @GetMapping("/busquedas/mayorfive")
+    public Boolean areStudentsMayorFive() {
+
+        return studentUseCase.getAllStudents().stream()
+                .allMatch(student -> student.getAge() > 5);
+    }
+
+    //Operaciones mas Avanzadas
+    @GetMapping("/operavanzadas/tenMoreYoung")
+    public List<Student> getStudentsMoreYoung() {
+
+        return studentUseCase.getAllStudents().stream()
+                .sorted( Comparator.comparing(Student::getAge))
+                .limit( 10)
+                .toList();
+    }
+
+    @GetMapping("/operavanzadas/porcentajeporetnia")
+    public Map<String, Double> getPercentageByEthnicGroup() {
+        List<Student> students = studentUseCase.getAllStudents();
+        int total = students.size();
+
+        Map<String, Long> countByEthnic = students.stream()
+                .collect(Collectors.groupingBy(Student::getEthnicGroup , Collectors.counting()));
+
+
+        return countByEthnic.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> (double) ((entry.getValue() * 100)/total)
+                ));
+    }
+
+    @GetMapping("/operavanzadas/promedioedad")
+    public Map<Integer, Double> getAgeAverageByGroup() {
+
+        return studentUseCase.getAllStudents().stream()
+                .collect(Collectors.groupingBy(Student::getGrade , Collectors.averagingInt(Student::getAge)));
+
+    }
+
+    @GetMapping("/operavanzadas/totalgeneroporcadagrado")
+    public Map<Integer, Map<String, Long>> getTotalByGenderInEachGrade() {
+        List<Student> students = studentUseCase.getAllStudents();
+
+
+        return students.stream()
+                .collect(Collectors.groupingBy(Student::getGrade , Collectors.groupingBy(Student::getGender, Collectors.counting())));
+
+    }
+
 }
